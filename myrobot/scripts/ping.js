71cefcb334a173d83e9aa75bdb9c7f6777105cc0
張  春雪 <zhangchunxue@zhangdeMacBook-Pro.local>
2022-02-17 16:57:31 +0900

    post request

--- myrobot/scripts/ping.js
+++ myrobot/scripts/ping.js
@@ -8,8 +8,49 @@
 'use strict';
 
 module.exports = (robot) => {
+
+  robot.respond(/(.*)$/i, (res) => {
+	res.send('メッセージありがとうございます！\n次の配信までお待ちください。');
+
+    //POST Request START
+	const https = require('https');
+	const { decycle, encycle } = require('json-cyclic');
+	const yourhost = 'yourhost.co.jp';
+	const yourpath = '/your/path';
+
+	var postData = JSON.stringify({
+		'from' : 'Direct',
+		'res' : decycle(res)
+	});
+	var options = {
+	hostname: yourhost,
+	port: 443,
+	path: yourpath,
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Content-Length': postData.length
+		}
+	};
+	var req = https.request(options, (res) => {
+		console.log('statusCode:', res.statusCode);
+		console.log('headers:', res.headers);
+		res.on('data', (d) => {
+			process.stdout.write(d);
+		});
+	});
+	req.on('error', (e) => {
+		console.error(e);
+	});
+	req.write(postData);
+	req.end();
+    //POST Request END
+
+  });
+
   robot.respond(/PING$/i, (res) => {
-    res.send(ws._socket.remoteAddress);
+    res.send('PONG in Heroku2');
+
   });
 
   robot.respond(/ADAPTER$/i, (res) => {
